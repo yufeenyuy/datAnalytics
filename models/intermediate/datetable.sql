e3ca32e47820a0596c46e4fe3a050b001070d228
Yufenyuy Tardzenyuy <tar.yufenyuy@gmail.com>
2024-01-27 13:14:50 +0100

    modified data transformation

--- models/intermediate/datetable.sql
+++ models/intermediate/datetable.sql
@@ -5,7 +5,7 @@ select
 to_char("date",'Mon-YYYY') date_my,
 to_char("date",'YYYYMM')::integer date_my_reorder,
 to_char("date",'DD-Mon') date_dm,
-to_char("date",'YYYYMMDD')::integer date_dm_reorder,
+to_char("date",'MMDD')::integer date_dm_reorder,
 to_char("date",'YYYY')::integer date_y,
 to_char("date", 'Mon') date_m,
 to_char("date",'DD') date_dnum,
